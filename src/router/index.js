a1158b8f43c5877eecaebf78407238a5730db05e
LaoDu <2453755913.@qq.com>
2020-10-10 15:39:41 +0800

    配置登录页路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,17 @@ Vue.use(VueRouter)
 
 import Home from '../views/Home.vue'
 
+import Login from "@/views/Login";
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-
-
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
   }
 ]
 
